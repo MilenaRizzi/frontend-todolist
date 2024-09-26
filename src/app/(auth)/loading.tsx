4aea5e1798b54a8eb364ee1636e47000bc990643
Your Name <milenaclararizzi@gmail.com>
2024-09-26 09:28:07 -0300

    Create loading.tsx

---
+++ src/app/(auth)/loading.tsx
@@ -0,0 +1,12 @@
+export default function AuthLoading() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="flex flex-col items-center space-y-4">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+        <p className="text-lg font-semibold text-gray-600">
+          Carregando, por favor aguarde...
+        </p>
+      </div>
+    </div>
+  );
+}
