c28233e401bc937eb980282d2abf217b8fbee5fa
Your Name <milenaclararizzi@gmail.com>
2024-09-26 09:28:31 -0300

    Delete layout.tsx

--- src/app/dashboard/layout.tsx
+++
@@ -1,8 +0,0 @@
-import Header from "@/components/header";
-import Link from "next/link";
-import { ReactNode } from "react";
-
-export default function DashboardLayout({ children }: { children: ReactNode }) {
-  return <div className="p-20">{children}</div>;
-}
-
